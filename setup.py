98116883a8aed44e5459ee2f1052f1db17442231
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-05-01 12:52:50 +0000

    make sure to require the newer version of grokcore.component

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'grokcore.component',
+        'grokcore.component >= 2.5dev',
         'grokcore.security',
         'grokcore.view',
         'grokcore.traverser',
