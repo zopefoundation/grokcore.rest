814faafcb41cc50e56a20c7485b83b97c94e7dfb
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-10 11:39:36 +0100

    Preparing release 3.0.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='grokcore.rest',
-    version='1.4.dev0',
+    version='3.0.0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
